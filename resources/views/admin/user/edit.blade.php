a93f9fcb90f161dd149dc9398add5c83bdd02ab6
arpit jain <arpitjain364@gmail.com>
2020-04-07 17:24:55 +0530

    215

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -3,36 +3,41 @@
 @section('content')
 
     <h1>Edit User</h1>
-    {!! Form::model($user, ['action' => ['AdminUsersController@update', $user->id], 'method' => 'PATCH', 'files'=>true]) !!}
-    <div class="form-group">
-        {!! Form::label('name', 'Name:') !!}
-        {!! Form::text('name', null, ['class' => 'form-control']) !!}
+    <div class="col-sm-3">
+        <img src="{{$user->photo ? $user->photo->file : 'http://placehold.it/40/40'}}" alt="" class="img-responsive img-rounded">
     </div>
-    <div class="form-group">
-        {!! Form::label('email', 'Email:') !!}
-        {!! Form::text('email', null, ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('role_id', 'Role:') !!}
-        {!! Form::select('role_id',[''=>'Choose Option'] + $roles , null, ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('is_active', 'Status:') !!}
-        {!! Form::select('is_active', array(1=>'Active', 0=>'Not Active'),null, ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('password', 'Password') !!}
-        {!! Form::text('password', '', ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('photo_id', 'File:') !!}
-        {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
-    </div>
+    <div class="col-sm-9">
+        {!! Form::model($user, ['action' => ['AdminUsersController@update', $user->id], 'method' => 'PATCH', 'files'=>true]) !!}
+        <div class="form-group">
+            {!! Form::label('name', 'Name:') !!}
+            {!! Form::text('name', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('email', 'Email:') !!}
+            {!! Form::text('email', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('role_id', 'Role:') !!}
+            {!! Form::select('role_id',[''=>'Choose Option'] + $roles , null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('is_active', 'Status:') !!}
+            {!! Form::select('is_active', array(1=>'Active', 0=>'Not Active'),null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('password', 'Password') !!}
+            {!! Form::text('password', '', ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('photo_id', 'File:') !!}
+            {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
+        </div>
 
-    <div class="form-group">
-        {!! Form::submit('Create User', ['class' => 'btn btn-primary']) !!}
+        <div class="form-group">
+            {!! Form::submit('Create User', ['class' => 'btn btn-primary']) !!}
+        </div>
+        {!! Form::close() !!}
     </div>
-    {!! Form::close() !!}
 
 
     @include('includes.form_error')
