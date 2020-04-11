71ea169b8a8f039a0b9275d36ce87b71fe6f0ebe
arpit jain <arpitjain364@gmail.com>
2020-04-11 20:20:24 +0530

    creating post form part 1

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -2,4 +2,25 @@
 
 @section('content');
 <h1>Create Post</h1>
+    {!! Form::open(['action' => 'AdminPostsController@store', 'method' => 'POST']) !!}
+    	<div class="form-group">
+            {!! Form::label('title', 'Title') !!}
+            {!! Form::text('titile', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('category_id', 'Category') !!}
+            {!! Form::select('category_id', array(''=>'option'),null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('photo_id', 'Photo') !!}
+            {!! Form::file('photo_id', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('body', 'Description') !!}
+            {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::submit('Create Post', ['class' => 'btn btn-primary']) !!}
+        </div>
+    {!! Form::close() !!}
 @stop
